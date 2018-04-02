b24782b1b89ccef70cacc9e5b2aa224b1d316029
Giacomo <jacob_g_92@live.nl>
2018-04-03 00:06:08 +0200

    update

--- opdracht4/v1/js/script.js
+++ opdracht4/v1/js/script.js
@@ -11,6 +11,7 @@ var request = new XMLHttpRequest();
 
 var title = document.querySelector('h1');
 var description = document.querySelector('p');
+var image = document.querySelector('img');
 
 request.open('GET', 'http://dennistel.nl/movies', true);
 request.onload = function () {
@@ -25,20 +26,26 @@ request.onload = function () {
         
         const h1 = document.createElement('h1');
         h1.textContent = movie.title;
-        
-//        const p = document.createElement('p');
-//        p.textContent = movie.plot;
-        
+                
         const img = document.createElement('img');
         img.src = 'http://ia.media-imdb.com/images/M/MV5BNTQ3OTkwNTgyN15BMl5BanBnXkFtZTcwNTAzOTAzOQ@@._V1_SX214_.jpg';
+    
+        const button = document.createElement('button');
+        button.setAttribute('class', 'button');
+        
+        const subcart = document.createElement('div');
+        subcart.setAttribute('class', 'subcard');
+        
+        const p = document.createElement('p');
+        p.textContent = movie.plot;
         
        
         
         container.appendChild(card);
         card.appendChild(h1);
-//        card.appendChild(p);
         card.appendChild(img);
-        
+        card.appendChild(button);
+        subcart.appendChild(p);
       
 
 
@@ -51,3 +58,24 @@ request.onload = function () {
 }
 
 request.send();
+
+
+
+
+
+var harmonica = document.getElementsByClassName("harmonica");
+
+for (var i = 0; i < harmonica.length; i++) {
+    harmonica[i].onclick = function () {
+        
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            //harmonica is nu open, dicht maar!
+            content.style.maxHeight = null;
+
+        } else {
+            //harmonica is dicht, openen!
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
+}
